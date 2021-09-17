5088a1afab47a1be281e180fa0e50596f5462b0b
davpoolechem <davpoole@iastate.edu>
2021-09-17 00:06:13 -0500

    Update to recipe to use internal conda compilers

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_C_COMPILER=x86_64-conda-linux-gnu-gcc -DCMAKE_CXX_COMPILER=x86_64-conda-linux-gnu-g++ .. 
 make -j`nproc`
 make install
